8fab7ccfd15be897aa38eb277e11e64dabc93dea
VenusBridget <venusbri27@gmail.com>
2024-05-23 22:20:36 +0300

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ('Hello, programmer!')
+greet_programmer()
 
 def greet(name):
-    pass
+    print (f'Hello, {name}!')
+greet ('Bridget')
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f'Hello, {name}!')
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    print (num1 + num2)
+    return (num1 + num2)
+add (2,2)
 
 def halve(number):
     pass
+    print (number/2)
+    return number/2
+halve(4)
